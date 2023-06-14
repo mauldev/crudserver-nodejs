378821e4964bca61b1d9fc242a3409c8bb171d8a
Ismail <mailmaul.dev@gmail.com>
2023-06-14 12:18:56 +0700

    fixing post error

--- index.js
+++ index.js
@@ -35,24 +35,24 @@ mongoose.connect("mongodb+srv://mailmaul:Maul123@mauldev.834ppjk.mongodb.net/flu
 )
 
 
-app.post("/api/add_product", (req,res)=>{
-    console.log("Result", req.body);
-    const pdata = {
-        "id": productData.length+1,
-        "pname": req.body.pname,
-        "pprice": req.body.pprice,
-        "pdesc": req.body.pdesc,
-    };
+// app.post("/api/add_product", (req,res)=>{
+//     console.log("Result", req.body);
+//     const pdata = {
+//         "id": productData.length+1,
+//         "pname": req.body.pname,
+//         "pprice": req.body.pprice,
+//         "pdesc": req.body.pdesc,
+//     };
 
-    productData.push(pdata);
-    console.log("Final", pdata);
+//     productData.push(pdata);
+//     console.log("Final", pdata);
 
-    res.status(200).send({
-        "status_code": 200,
-        "message": "Product added successfully",
-        "product": pdata
-    });
-})
+//     res.status(200).send({
+//         "status_code": 200,
+//         "message": "Product added successfully",
+//         "product": pdata
+//     });
+// })
 
 app.get("/api/get_product", (req,res) => {
     if(productData.length >0){
