d77711299a55d80dae9e68bb4e2a9f2bf0679f74
Ismail <mailmaul.dev@gmail.com>
2023-06-14 13:20:34 +0700

    get product

--- index.js
+++ index.js
@@ -15,44 +15,22 @@ app.listen(port, ()=>{
     console.log(`Connected to server at ${port}`);
 });
 
-app.get('/', (req,res)=>{
-    res.send('Hello World');
-    })
+
 
 mongoose.set('strictQuery',true);
 mongoose.connect("mongodb+srv://mailmaul:Maul123@mauldev.834ppjk.mongodb.net/flutter",
 (error) => {
     if(!error){
         console.log("Status", "Connected to mongoose")
-
-        app.get('/', (req,res)=>{
-            res.send('Hello World');
-            })
-
-        app.post("/api/add_product", async (req,res) => {
-            console.log("Result", req.body);
-
-            let data = Product(req.body);
-
-            try{
-                let dataToStore = await data.save();
-                res.status(200).json(dataToStore);
-            } catch (error) {
-                res.status(400).json({
-                    'status': error.message
-                })
-            }
-
-            app.use((req, res) => {
-                res.status(404).send("Not Found");
-              });
-        })
     } else {
         console.log(error.message);
     }
 }
-
 )
+
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+    })
 
 app.post("/api/add_product", async (req,res) => {
     console.log("Result", req.body);
@@ -64,6 +42,22 @@ app.post("/api/add_product", async (req,res) => {
         res.status(200).json(dataToStore);
     } catch (error) {
         res.status(400).json({
+            'status': error.message
+        })
+    }
+
+    app.use((req, res) => {
+        res.status(404).send("Not Found");
+      });
+})
+
+app.get("/api/get_product", async (req,res) => {
+
+    try{
+        let data = await Product.find();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({
             'status': error.message
         })
     }
